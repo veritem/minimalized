a1ee772055f9134eb0b813a98b1a78759196839b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-07-03 18:58:28 +0200

    fix: setup example

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,18 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Minimalized } from '../src/index';
+import { useState } from 'react';
 
 const App = () => {
+  const [open, setIsOpen] = useState<boolean>(false);
   return (
-    <div>
-      <Thing />
-    </div>
+    <section>
+      <button onClick={() => setIsOpen(true)}>Open modal</button>
+      <Minimalized open={open} close={() => setIsOpen(!open)}>
+        <h1>HI</h1>
+      </Minimalized>
+    </section>
   );
 };
 
